40f61d64a08fcd53f1ad24efd06b8238b5ecb0d0
Administrator <Administrator@MSDN-SPECIAL>
2019-01-21 00:55:11 +0900

    6강

---
+++ src/TestMainClass.java
@@ -0,0 +1,138 @@
+
+public class TestMainClass {
+	
+			public static void main(String[] args) {
+			/*	
+				int x =10;
+				int y =20;
+				
+				//대입 연산자
+				System.out.println("x = "+x);
+				System.out.println("y = "+y);
+				
+				
+				x = y;
+				System.out.println("x = " + x);
+				System.out.println("y = " + y); 
+				
+				*/
+				
+			/*	//산술 연산자
+				
+				int x = 10;
+				int y = 20;
+				
+				System.out.println("x + y ="+(x+y));
+				System.out.println("x + y ="+(x-y));
+				System.out.println("x + y ="+(x*y));
+				System.out.println("x + y ="+(x/y));
+				System.out.println("x + y ="+(x%y)); // 나누기 몫은 나누는 나눌 값이 더 작으면 무조건 나누어 지는 값이 무조건 나머지 값이다.
+				
+				*/
+				
+				
+			/*	// 복합 대입연산자
+				int x = 10;
+				System.out.println("x += 10 " + (x +=10)); // x = x + 10;
+				x = 10;
+				System.out.println("x += 10 " + (x -=10)); // x = x - 10;
+				x = 10;
+				System.out.println("x += 10 " + (x *=10)); // x = x * 10;
+				x = 10;
+				System.out.println("x += 10 " + (x /=10)); // x = x / 10;
+				x = 10;
+				System.out.println("x += 10 " + (x %=10)); // x = x % 10;
+				*/
+		
+			
+				/*// 관계 연산자
+				
+				int x =10; int y = 20;
+				
+				System.out.println("x > y : " + (x > y)); // 관계 연산자는 왼쪽이 기준이다. 참 : true 거짓 : false
+				System.out.println("x < y : " + (x < y)); // 관계 연산자는 왼쪽이 기준이다.
+				System.out.println("x >= y : " + (x >= y)); // 관계 연산자는 왼쪽이 기준이다.
+				System.out.println("x <= y : " + (x <= y)); // 관계 연산자는 왼쪽이 기준이다.
+				System.out.println("x == y : " + (x == y)); // 관계 연산자는 왼쪽이 기준이다.
+				System.out.println("x != y : " + (x != y)); // 관계 연산자는 왼쪽이 기준이다.
+					
+				// 1.false 2.true 3.false 4.true 5.false 6.true*/
+				
+				
+			/*	// 증감 연산자
+				
+				
+				//  -선입연산자 // 연산 후 출력
+				
+				int x = 10;
+				System.out.println("++x : "+(++x));
+		
+				x = 10;
+				System.out.println("--x : "+(--x));
+				
+				
+			   //   -후입연산자 // 출력 후 연산
+				x = 10;
+				
+				System.out.println("x++ : "+(x++));
+				System.out.println("x : "+x);
+				
+				x = 10;
+				System.out.println("x-- : "+(x--));
+				System.out.println("x : "+ x);
+				*/
+				
+				
+				/*//논리 연산자 
+				
+				boolean b1 = false ;
+				boolean b2 = true ;
+				
+				System.out.println("bl && b2 : "+(b1 && b2));
+				System.out.println("bl || b2 : "+(b1 || b2));
+				System.out.println("!bl: "+(!b1));
+				System.out.println("!b2: "+(!b2));
+				*/
+				
+				
+				/*//조건 (삼항) 연산자
+				
+				int x = 10; int y = 20;
+				int result = 0;
+				result = (x > y) ? 100 : 200; // 조건식 ? 식 1 : 식 2   참이면 식1 실행 ,, 거짓이면 식2 실행
+				System.out.println("result : "+ result);
+				
+				result = (x < y) ? 100 : 200; // 조건식 ? 식 1 : 식 2   참이면 식1 실행 ,, 거짓이면 식2 실행
+				System.out.println("result : "+ result);
+				
+				result = (x == y) ? 100 : 200; // 조건식 ? 식 1 : 식 2   참이면 식1 실행 ,, 거짓이면 식2 실행
+				System.out.println("result : "+ result);
+				
+					// 답 : 200, 100, 200
+				
+				*/
+				
+			/*	// 비트 연산자
+				//0010
+				//0011
+				
+				
+				int x = 2;
+				int y = 3;
+				
+				System.out.println("x & y  = "+(x & y)); //2 (AND)
+				System.out.println("x | y  = "+(x | y)); //3 (OR)
+				System.out.println("x ^ y  = "+(x ^ y)); // (XOR같지 않으면 1) 1
+				
+				
+				
+				
+				
+				
+				*/
+				
+			}
+	
+	
+	
+}
